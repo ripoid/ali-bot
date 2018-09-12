91eeb3292d6dab65c4d41a0ac78701ba7bb66853
Twilight <amfromh@hotmail.com>
2018-09-13 01:38:19 +0800

    add cv-demo

---
+++ cv-demo.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+def fun():
+    cap =  cv2.VideoCapture(1)
+    while (True):
+        (ret, frame) =cap.read()
+        cv2.imshow('frame', frame)
+
+        if (cv2.waitKey(1) & 0xFF==ord('q')):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+fun()
